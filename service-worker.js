521964e4e80c057206ec783c592d847b6247ab55
marcoc82 <marco.ery2010@gmail.com>
2026-01-08 14:09:22 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome della cache
-const CACHE_NAME = 'polis-convocazioni-v9.67';
+const CACHE_NAME = 'polis-convocazioni-v9.68';
 // File da mettere in cache
 const urlsToCache = [
   './index.html',
