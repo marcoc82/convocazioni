14e7fae5f54c73752545bf25c324779fcb811800
marcoc82 <marco.ery2010@gmail.com>
2026-02-01 15:34:15 +0100

    Add files via upload
    AGGIUNTO NELLO STORICO CONVOCAZIONI IL DEFAULT PER SETTIMANA CORRENTE

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome della cache
-const CACHE_NAME = 'polis-convocazioni-v9.73';
+const CACHE_NAME = 'polis-convocazioni-v9.74';
 // File da mettere in cache
 const urlsToCache = [
   './index.html',
