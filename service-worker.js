83d46fd46f501c493aa26aa6adfd5e14a203de0d
marcoc82 <marco.ery2010@gmail.com>
2026-02-20 10:25:12 +0100

    Update cache name and maintain service worker logic

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome della cache ANCHE SE NON MI PIACE
-const CACHE_NAME = 'polis-convocazioni-v9.91';
+const CACHE_NAME = 'polis-convocazioni-v9.94';
 // File da mettere in cache
 const urlsToCache = [
   './index.html',
@@ -78,6 +78,7 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
 
 
 
